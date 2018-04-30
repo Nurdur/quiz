94e976a07db2985d0942ffc33421d060dc3eaf33
Milan Keser <milan@kesers.com>
2018-04-30 16:22:35 +0200

    Basic functionality. Reads default file, quizzes user, calculates score.

--- quiz.go
+++ quiz.go
@@ -18,6 +18,9 @@ func main() {
 	// Create a reader
 	r := csv.NewReader(bufio.NewReader(f))
 	q := bufio.NewReader(os.Stdin)
+	score := 0
+	total := 0
+
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -26,12 +29,14 @@ func main() {
 		if len(record) != 2 {
 			fmt.Println("Line: ", record, "does not appear to be a question-answer pair. Skipping")
 		} else {
-			fmt.Println("Question: ", record[0])
-			fmt.Print("-> ")
+			fmt.Print(record[0], " = ")
 			text, _ := q.ReadString('\n')
 			text = strings.Replace(text, "\n", "", -1)
-			fmt.Println("Response: ", text)
-			fmt.Println("Answer: ", record[1])
+			total++
+			if text == record[1] {
+				score++
+			} 
 		}
 	}
+	fmt.Printf("Score: %d out of %d correct. That's %.2f%%\n", score, total, 100.0*float64(score)/float64(total))
 }
